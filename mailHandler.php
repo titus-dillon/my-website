8bfb96a03837664ab933f832b1713ebdb495506f
titus-dillon <tidillon28@gmail.com>
2016-04-25 19:18:11 -0500

    Removed some comments

--- mailHandler.php
+++ mailHandler.php
@@ -1,22 +1,11 @@
 <?php
 
-/*
-error_reporting(-1);
-ini_set('display_errors', 'On');
-set_error_handler("var_dump");
-*/
-
 $recipient = "tidillon28@gmail.com";
 
 $sender = $_POST['uname'];
 $uemail = $_POST['email'];
 $subject = $_POST['subject'];
 $msg = $_POST['msg'];
-
-/*echo "[$sender]\n";
-echo "[$uemail]\n";
-echo "[$subject]\n";
-echo "[$msg]";*/
 
 $msg .= "\n\n";
 $msg .= $sender;
